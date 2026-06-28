22aaefed569062aa2bc5ac9e8e368d7e6b8b4668
Макс <hspu1@tuta.io>
2026-06-28 19:28:39 +0300

    feat: step2 postgres impl (+ related)

---
+++ src/infra/persistence/models/mixins.py
@@ -0,0 +1,31 @@
+from datetime import datetime
+from uuid import UUID
+
+from sqlalchemy import DateTime, Uuid, func
+from sqlalchemy.orm import Mapped, mapped_column
+from uuid6 import uuid7
+
+
+class TimestampMixin:
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        server_default=func.now(),
+        nullable=False,
+        sort_order=999,
+    )
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        server_default=func.now(),
+        onupdate=func.now(),
+        nullable=False,
+        sort_order=1000,
+    )  # manual use (if needed)
+
+
+class UUIDv7Mixin:
+    id: Mapped[UUID] = mapped_column(
+        Uuid,
+        primary_key=True,
+        default=uuid7,
+        sort_order=-100,
+    )
