552b5169db1fce087ef4e71612e7f3e748124f83
Hspu1 <hspu1@tuta.io>
2026-08-08 15:09:49 +0000

    fix(codebase): use correct paths

--- deploy/alembic/migrations/env.py
+++ deploy/alembic/migrations/env.py
@@ -6,9 +6,9 @@ from sqlalchemy import pool
 from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import async_engine_from_config
 
-from iwe.core.env_conf import pg_stg
-from iwe.shared.postgres.base import Base
-from iwe.shared.postgres.schema import (
+from iwe.shared.env_conf import pg_stg
+from iwe.infra.postgres.base import Base
+from iwe.infra.postgres.schema import (
     UsersModel,
     WalletsModel,
     UserCardsModel,
--- src/iwe/iwe/healthz.py
+++ src/iwe/iwe/healthz.py
@@ -2,16 +2,17 @@ import asyncio
 
 from fastapi import APIRouter, HTTPException, status
 
-from iwe.shared.dependencies import pg_manager
+from iwe.shared import dependencies
 
 healthz_router = APIRouter(prefix="/healthz", tags=["[System]"])
 
 
 @healthz_router.get("/readiness", status_code=status.HTTP_200_OK)
 async def readiness() -> dict[str, str]:
+    pg = dependencies.pg_manager
     try:
         async with asyncio.timeout(5.0):
-            await pg_manager.ping()
+            await pg.ping()
 
         print("[INFO] [HEALTHZ] allez", flush=True)
         return {
